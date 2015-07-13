62ff9803a65346ef25f27511e08582405274478d
Patrick Mowrer <pmowrer@gmail.com>
2015-07-13 11:46:37 -0400

    Add failing test for an invalid identifier name.

--- test.js
+++ test.js
@@ -7,3 +7,8 @@ test(function (t) {
   t.equal(globalize('foo'), '(typeof window !== "undefined" ? window.foo : typeof global !== "undefined" ? global.foo : null)')
   t.end()
 })
+
+test(function (t) {
+  t.equal(globalize('foo-bar'), '(typeof window !== "undefined" ? window[\'foo-bar\'] : typeof global !== "undefined" ? global[\'foo-bar\'] : null)')
+  t.end()
+})
