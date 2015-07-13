e429ae05a468699838551250cd25c062929e4a9e
Patrick Mowrer <pmowrer@gmail.com>
2015-07-13 11:51:52 -0400

    Use bracket notation instead of dot notation for all identifiers.
    Could try to use dot notation for all valid identifier names, but
    doesn't seem worth the trouble.
    
    [Fixes #1]

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var printf = require('pff')
 module.exports = function globalize (id) {
   var hasWindow = 'typeof window !== "undefined"'
   var hasGlobal = 'typeof global !== "undefined"'
-  return printf('(%s ? window.%s : %s ? global.%s : null)', hasWindow, id, hasGlobal, id)
+  return printf('(%s ? window[\'%s\'] : %s ? global[\'%s\'] : null)', hasWindow, id, hasGlobal, id)
 }
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var test = require('tape')
 var globalize = require('./')
 
 test(function (t) {
-  t.equal(globalize('foo'), '(typeof window !== "undefined" ? window.foo : typeof global !== "undefined" ? global.foo : null)')
+  t.equal(globalize('foo'), '(typeof window !== "undefined" ? window[\'foo\'] : typeof global !== "undefined" ? global[\'foo\'] : null)')
   t.end()
 })
 
