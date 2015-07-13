d6707d81829213afc004dd7d77e28a90db7e9596
Ben Drucker <bvdrucker@gmail.com>
2015-07-13 15:29:10 -0400

    Treat dots as accessor paths and allow dots to be escaped

--- test.js
+++ test.js
@@ -5,10 +5,8 @@ var globalize = require('./')
 
 test(function (t) {
   t.equal(globalize('foo'), '(typeof window !== "undefined" ? window[\'foo\'] : typeof global !== "undefined" ? global[\'foo\'] : null)')
-  t.end()
-})
-
-test(function (t) {
   t.equal(globalize('foo-bar'), '(typeof window !== "undefined" ? window[\'foo-bar\'] : typeof global !== "undefined" ? global[\'foo-bar\'] : null)')
+  t.equal(globalize('foo.bar'), '(typeof window !== "undefined" ? window[\'foo\'][\'bar\'] : typeof global !== "undefined" ? global[\'foo\'][\'bar\'] : null)')
+  t.equal(globalize('foo\\.bar'), '(typeof window !== "undefined" ? window[\'foo.bar\'] : typeof global !== "undefined" ? global[\'foo.bar\'] : null)')
   t.end()
 })
